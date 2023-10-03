1ec0566da5947ef3540a94ac5c25855bff16f243
Pavel Paranin <pavel@paran.in>
2023-10-03 11:17:37 +0200

    feat: mass selection identation is added

--- lua/configs/remaps.lua
+++ lua/configs/remaps.lua
@@ -12,3 +12,6 @@ vim.keymap.set("n", "<A-k>", "<Cmd>m .-2<CR>")
 vim.keymap.set("n", "<A-j>", "<Cmd>m .+1<CR>")
 vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv")
+
+vim.keymap.set("v", ">", ">gv")
+vim.keymap.set("v", "<", "<gv")
