de81b7be07e4003d87cff7313fcd5cca8af09bc2
Pavel Paranin <pavel@paran.in>
2023-12-06 17:54:00 +0100

    feat: call TT for one-time toggleterm command

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -9,3 +9,25 @@ require("toggleterm").setup {
         end
     end
 }
+
+local function run_in_toggleterm(args)
+    local command  = args.args
+
+    local on_open  = function(term)
+        -- simulate the following key press sequence: <C-\><C-n><C-w>p
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n><C-w>p", true, true, true), "n", true)
+    end
+
+    local Terminal = require('toggleterm.terminal').Terminal
+    local shell    = Terminal:new({ cmd = command, hidden = true, on_open = on_open })
+
+    shell:toggle()
+end
+
+vim.api.nvim_create_user_command(
+    'TT',
+    run_in_toggleterm,
+    { nargs = "*", desc = "Run command in a new ToggleTerm terminal" }
+)
+
+vim.keymap.set("n", "<leader>tt", ":TT ");
