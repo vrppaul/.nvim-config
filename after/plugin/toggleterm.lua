a8e17d8fb1318c3ebd9c2fcaa74fed198ab59206
Pavel Paranin <pavel@paran.in>
2023-12-07 16:37:17 +0100

    fix: correct open of toggleterm through custom command

--- after/plugin/toggleterm.lua
+++ after/plugin/toggleterm.lua
@@ -11,17 +11,9 @@ require("toggleterm").setup {
 }
 
 local function run_in_toggleterm(args)
-    local command  = args.args
+    local command = args.args
 
-    local on_open  = function(term)
-        -- simulate the following key press sequence: <C-\><C-n><C-w>p
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-\\><C-n><C-w>p", true, true, true), "n", true)
-    end
-
-    local Terminal = require('toggleterm.terminal').Terminal
-    local shell    = Terminal:new({ cmd = command, hidden = true, on_open = on_open })
-
-    shell:toggle()
+    vim.cmd("TermExec cmd='" .. command .. "'")
 end
 
 vim.api.nvim_create_user_command(
