179568951e573d3559cbfa6eff6c53f748b02415
Pavel Paranin <pavel@paran.in>
2023-12-07 13:05:08 +0100

    feat: new keymap to show all stashes

--- after/plugin/telesope.lua
+++ after/plugin/telesope.lua
@@ -6,4 +6,5 @@ require('telescope').setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<leader>gf', builtin.git_status, {})
 vim.keymap.set('n', '<leader>lg', builtin.live_grep, {})
